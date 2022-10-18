20a3b5cadf6bc1dd4912e5e490c9def2de39a01c
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-10-18 00:57:26 -0700

    add li element

--- data.js
+++ data.js
@@ -1,1 +1,11 @@
 // Your code here
+const nameElement = document.querySelector('#name'),
+  add = document.querySelector('#add'),
+  ul = document.querySelector('#shopping-list');
+
+add.addEventListener('click', e => {
+  e.preventDefault();
+  const li = document.createElement('li');
+  li.textContent = nameElement.value;
+  ul.appendChild(li);
+});
