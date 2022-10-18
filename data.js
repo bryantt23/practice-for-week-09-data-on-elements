5563833b598dcace31be4e46c4e45bce65b65ddf
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-10-18 01:03:41 -0700

    color based on data type

--- data.js
+++ data.js
@@ -1,11 +1,13 @@
 // Your code here
 const nameElement = document.querySelector('#name'),
   add = document.querySelector('#add'),
-  ul = document.querySelector('#shopping-list');
+  ul = document.querySelector('#shopping-list'),
+  type = document.querySelector('#type');
 
 add.addEventListener('click', e => {
   e.preventDefault();
   const li = document.createElement('li');
   li.textContent = nameElement.value;
+  li.setAttribute('data-type', type.value);
   ul.appendChild(li);
 });
